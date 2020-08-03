a948e2f1772e5db00596fc3d14dd6bc47220d8ff
Anshu Prakash <anshuprakash8445@gmail.com>
2020-08-03 16:55:17 +0530

    Created Serializers

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person, Country, State, City
 
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
+        fields = '__all__'
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = '__all__'
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
         fields = '__all__'
