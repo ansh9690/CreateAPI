19071ba5530d7c0e1c8ed6e292f76cc84fec7699
Anshu Prakash <anshuprakash8445@gmail.com>
2020-08-03 16:53:00 +0530

    Created all Model

--- website/api/admin.py
+++ website/api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Country, State, City
 
 admin.site.register(Person)
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
--- website/api/models.py
+++ website/api/models.py
@@ -6,3 +6,48 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Person"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    population = models.IntegerField(default=0)
+    gdp = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class State(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    population = models.IntegerField(default=0)
+    gdp = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "States"
+
+
+class City(models.Model):
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    population = models.IntegerField(default=0)
+    gdp = models.FloatField(default=0)
+    pin_code = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.pin_code
+
+    class Meta:
+        verbose_name_plural = "Cities"
